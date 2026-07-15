81f15a5ea3dac896a44b37efaebb046bd7fffbb1
Sebastian Fichtner <sebastian@nohype.ai>
2026-07-15 11:45:51 +0200

    Prepare making TreeSitter path independent of LSP path

--- Code/App/TreeSitter Codebase/Load/CreateTreeSitterCodebaseFromLSPCodebase.swift
+++ Code/App/TreeSitter Codebase/Load/CreateTreeSitterCodebaseFromLSPCodebase.swift
@@ -1,19 +1,24 @@
 import Foundation
+import SwiftLSP
 
 enum CreateTreeSitterCodebaseFromLSPCodebase {
-    static func extract(from codeFolder: LSPCodeFolder) throws -> TreeSitterFolder {
-        try project(codeFolder)
+    static func load(fromFolder folder: LSP.CodebaseLocation) throws -> TreeSitterFolder {
+        throw "not implemented yet"
     }
     
-    private static func project(_ folder: LSPCodeFolder) throws -> TreeSitterFolder {
-        let files = try (folder.files ?? []).map(project(file:))
-        let subfolders = try (folder.subfolders ?? []).map(project)
+    static func extract(from codeFolder: LSPCodeFolder) throws -> TreeSitterFolder {
+        try convert(codeFolder)
+    }
+    
+    private static func convert(_ folder: LSPCodeFolder) throws -> TreeSitterFolder {
+        let files = try (folder.files ?? []).map(convert(file:))
+        let subfolders = try (folder.subfolders ?? []).map(convert)
         return TreeSitterFolder(name: folder.name,
                                 files: files,
                                 subfolders: subfolders)
     }
     
-    private static func project(file: LSPCodeFile) throws -> TreeSitterFile {
+    private static func convert(file: LSPCodeFile) throws -> TreeSitterFile {
         let ext = (file.name as NSString).pathExtension
         guard let language = SourceLanguage.from(fileExtension: ext) else {
             return TreeSitterFile(name: file.name, code: file.code, nodes: [])
